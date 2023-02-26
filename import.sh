228be6f8edaeb518b2b8f41a98f64acb4b1a1200
wwagner4 <wwagner4@gmail.com>
2023-02-26 17:09:55 +0100

    added rm on import.sh

--- import.sh
+++ import.sh
@@ -1,3 +1,5 @@
 docker run -v /home/wwagner4/prj/fahrplan/data:/data \
---network host gtfsdb --database_url  postgresql://docker:docker@localhost:5432/gtfsdb \
+--rm \
+--network host gtfsdb \
+--database_url postgresql://docker:docker@localhost:5432/gtfsdb \
 /data/20230224-0154_gtfs_vor_2023.zip
