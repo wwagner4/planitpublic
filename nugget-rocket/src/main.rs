9e2a570b88c2cb2fdcda7038e925a3a28f660733
wwagner4 <wwagner4@gmail.com>
2023-03-04 18:04:51 +0100

    calling routs ok

--- nugget-rocket/src/main.rs
+++ nugget-rocket/src/main.rs
@@ -18,7 +18,7 @@ fn server() -> Rocket<Build> {
         .mount("/users", routes![get_users, get_user])
 }*/
 
-/*#[rocket::main]
+#[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let _rocket = rocket::build()
         .mount("/users", rocket::routes![users::get_users, users::get_user])
@@ -26,10 +26,11 @@ async fn main() -> Result<(), rocket::Error> {
         .await?;
 
     Ok(())
-}*/
+}
 
-#[tokio::main]
+
+/*#[tokyo::main] <- here you can use rocket also. seems to enable async as well
 async fn main() {
     some_postgres_sqlx_tries().await.unwrap();
 }
-
+*/
